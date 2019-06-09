3693c1dd952d19d5294c956ff33a343b367436d0
deveitrust <51053434+deveitrust@users.noreply.github.com>
2019-06-09 19:11:10 +0300

    Create sp_UnitTests_GetPrimaryKeys.sql

---
+++ sp_UnitTests_GetPrimaryKeys.sql
@@ -0,0 +1,11 @@
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[sp_UnitTests_GetPrimaryKeys]') AND type in (N'P', N'PC'))
+	DROP PROCEDURE [dbo].[sp_UnitTests_GetPrimaryKeys]
+GO
+
+CREATE Procedure [dbo].[sp_UnitTests_GetPrimaryKeys]
+AS
+	SELECT TABLE_NAME as TableName, COLUMN_NAME as ColumnName, DATA_TYPE as DataType
+	FROM INFORMATION_SCHEMA.COLUMNS
+	WHERE COLUMNPROPERTY(object_id(TABLE_SCHEMA+'.'+TABLE_NAME), COLUMN_NAME, 'IsIdentity') = 1 OR ORDINAL_POSITION = 1
+	ORDER BY TABLE_NAME
+GO
