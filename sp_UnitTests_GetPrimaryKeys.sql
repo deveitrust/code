fb4dd76dc7a67fec4a31905a1501807daf316234
deveitrust <51053434+deveitrust@users.noreply.github.com>
2019-06-12 23:00:00 +0300

    Update sp_UnitTests_GetPrimaryKeys.sql

--- sp_UnitTests_GetPrimaryKeys.sql
+++ sp_UnitTests_GetPrimaryKeys.sql
@@ -1,11 +1,15 @@
-IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[sp_UnitTests_GetPrimaryKeys]') AND type in (N'P', N'PC'))
-	DROP PROCEDURE [dbo].[sp_UnitTests_GetPrimaryKeys]
+IF EXISTS (
+    SELECT * FROM sys.objects 
+    WHERE object_id = OBJECT_ID(N'[dbo].[sp_UnitTests_GetPrimaryKeys]') 
+    AND type in (N'P', N'PC'))
+    DROP PROCEDURE [dbo].[sp_UnitTests_GetPrimaryKeys]
 GO
 
 CREATE Procedure [dbo].[sp_UnitTests_GetPrimaryKeys]
 AS
-	SELECT TABLE_NAME as TableName, COLUMN_NAME as ColumnName, DATA_TYPE as DataType
-	FROM INFORMATION_SCHEMA.COLUMNS
-	WHERE COLUMNPROPERTY(OBJECT_ID(TABLE_SCHEMA+'.'+TABLE_NAME), COLUMN_NAME, 'IsIdentity') = 1 OR ORDINAL_POSITION = 1
-	ORDER BY TABLE_NAME
+    SELECT TABLE_NAME as TableName, COLUMN_NAME as ColumnName, DATA_TYPE as DataType
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE COLUMNPROPERTY(OBJECT_ID(TABLE_SCHEMA+'.'+TABLE_NAME), COLUMN_NAME, 'IsIdentity')=1 
+    OR ORDINAL_POSITION=1
+    ORDER BY TABLE_NAME
 GO
